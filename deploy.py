a49b33691b9799e8adacde7cf9c86406c3b2a782
ajprax <ajprax@gmail.com>
2025-10-05 02:00:40 -0700

    tag when deploying

--- deploy.py
+++ deploy.py
@@ -1,11 +1,21 @@
+import tomllib
 from glob import glob
 from os import remove
 from os.path import dirname, join
 from subprocess import check_call
 
+
+def version():
+    with open("pyproject.toml", "rb") as f:
+        return tomllib.load(f)["project"]["version"]
+
+
 if __name__ == "__main__":
     for path in glob(join(dirname(__file__), "dist", "*")):
         remove(path)
 
+    version = version()
     check_call("python -m build".split())
     check_call("python -m twine upload --repository pypi dist/* --verbose".split())
+    check_call(f"git tag {version}".split())
+    check_call(f"git push origin {version}".split())
